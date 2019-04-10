ddb45f09616add6c3eaa2dc672d2f937100cdf7b
crowjonah <crowjonah@gmail.com>
2019-04-10 15:28:15 -0400

    try child-process-promise

--- index.js
+++ index.js
@@ -1,47 +1,58 @@
+/* eslint-env node */
 'use strict';
-
-let { spawn } = require('child_process');
-let RSVP = require('rsvp');
-let BasePlugin = require('ember-cli-deploy-plugin');
-let path = require('path');
-let surge = path.resolve(path.dirname(require.resolve('surge')), '../../.bin/surge');
+var BasePlugin = require('ember-cli-deploy-plugin');
+var RSVP = require('rsvp');
+var exec = require('child-process-promise');
+var path = require('path');
 
 module.exports = {
   name: "ember-cli-deploy-surge",
 
   createDeployPlugin: function(options) {
-    let DeployPlugin = BasePlugin.extend({
+    var DeployPlugin = BasePlugin.extend({
       name: options.name,
-      runAfter: 'build',
+
       defaultConfig: {
         project: 'dist/'
       },
       requiredConfig: ['domain'],
+      setup: function () {
+        this.log('setting `domain` and `project` in deployment context', { verbose: true });
+
+        return {
+          domain: this.readConfig('domain'),
+          prodject: this.readConfig('project')
+        }
+      }
+      configure: function (context) {
+        this.log('ember-cli-deploy-surge configure hook');
+      },
+      willDeploy: function (context) {
+        this.log('ember-cli-deploy-surge willDeploy hook');
+      },
       upload: function(context) {
-        let self = this;
-        let project = this.readConfig('project');
-        let domain = this.readConfig('domain');
+        var self = this;
+        var project = this.readConfig('project');
+        var domain = this.readConfig('domain');
 
         var done = this.async();
 
-        if (process.platform === 'win32') {
-          surge += '.cmd';
-        }
-
         this.log('preparing to upload `' + project + '` to surge project `' + domain + '`');
 
-        const surgeSpawn = spawn('surge', [project, domain]);
-
-        surgeSpawn.stdout.on('data', (data) => {
-          self.log(code);
-        });
-        surgeSpawn.stderr.on('data', (data) => {
-          self._errorMessage.bind(self)
-        });
-        surgeSpawn.on('close', (code) => {
-          self.log(code);
-          done();
-        });
+        return exec(`surge ${project} ${domain}`)
+          .then(function (result) {
+            var stdout = result.stdout
+            var stderr = result.stderr;
+            if (stderr) {
+              self.log(stderr, {color: 'red'});
+            }
+            if (stdout) {
+              self.log(stdout);
+            }
+          })
+          .catch(function (err) {
+            throw new Error (err.stack);
+          });
       },
       _errorMessage: function(error) {
         this.log(error, { color: 'red' });
