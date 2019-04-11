d3d4e2d63835788a806b0227979d7586beeb3d37
crowjonah <crowjonah@gmail.com>
2019-04-11 12:18:26 -0400

    fix package keyword issue, update plugin script from working project

--- index.js
+++ index.js
@@ -1,68 +1,66 @@
-/* eslint-env node */
 'use strict';
-var BasePlugin = require('ember-cli-deploy-plugin');
-var RSVP = require('rsvp');
-var exec = require('child-process-promise');
-var path = require('path');
+
+var BasePlugin = require('ember-cli-deploy-plugin')
+var RSVP = require('rsvp')
+var exec = require('child-process-promise').exec
+var path = require('path')
+var fs = require('fs')
 
 module.exports = {
-  name: "ember-cli-deploy-surge",
-
+  name: require('./package').name,
+  defaultConfig: { project: 'dist' },
+  requiredConfig: ['domain'],
   createDeployPlugin: function(options) {
     var DeployPlugin = BasePlugin.extend({
       name: options.name,
-
+      runAfter: 'build',
       defaultConfig: {
-        project: 'dist/'
+        environment: 'production',
+        project: 'tmp/deploy-dist/'
       },
       requiredConfig: ['domain'],
-      setup: function () {
-        this.log('setting `domain` and `project` in deployment context', { verbose: true });
+      willUpload: function(context) {
+        let self = this
+        let oldFile = path.join(self.readConfig('project'), 'index.html')
+        let newFile = path.join(self.readConfig('project'), '200.html')
+        let rename = new Promise((resolve, reject) => {
+          fs.rename(
+            oldFile,
+            newFile,
+            function (err) {
+              if (err != null) {
+                self.log(err, { color: red })
+                reject(err)
+              }
+              resolve(newFile)
+            })  
+        })
 
-        return {
-          domain: this.readConfig('domain'),
-          prodject: this.readConfig('project')
-        }
-      },
-      configure: function (context) {
-        this.log('ember-cli-deploy-surge configure hook');
-      },
-      willDeploy: function (context) {
-        this.log('ember-cli-deploy-surge willDeploy hook');
+        return rename;
       },
       upload: function(context) {
-        var self = this;
-        var project = this.readConfig('project');
-        var domain = this.readConfig('domain');
-
-        var done = this.async();
-
-        this.log('preparing to upload `' + project + '` to surge project `' + domain + '`');
+        var self = this
+        var project = this.readConfig('project')
+        var domain = this.readConfig('domain')
 
         return exec(`surge ${project} ${domain}`)
           .then(function (result) {
+            self.log('surge upload exec then, `domain`: ' + domain + '`project`: ' + project) // this logs, but no others
             var stdout = result.stdout
             var stderr = result.stderr;
             if (stderr) {
-              self.log(stderr, {color: 'red'});
+              self.log(stderr, {color: 'red'})
             }
             if (stdout) {
-              self.log(stdout);
+              self.log(stdout)
             }
           })
           .catch(function (err) {
-            throw new Error (err.stack);
-          });
-      },
-      _errorMessage: function(error) {
-        this.log(error, { color: 'red' });
-        if (error) {
-          this.log(error.stack, { color: 'red' });
-        }
-        return RSVP.reject(error);
+            throw new Error (err.stack)
+          })
       }
     });
 
-    return new DeployPlugin();
+    return new DeployPlugin()
   }
 };
